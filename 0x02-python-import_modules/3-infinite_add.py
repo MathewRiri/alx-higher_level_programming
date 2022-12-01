68d58a0e9c45492b73647ee4a88d8b5d9c2ffced
MathewRiri <mrnjoka254@gmail.com>
2022-12-01 22:04:36 +0300

    task3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    num_args = len(argv)
-    total = 0
-    for i in range(1, num_args):
-        total += int(argv[i])
-    print("{:d}".format(total))
+    import sys
+
+    argc = len(sys.argv)
+    add = 0
+    for j in range(1, argc):
+        add += int(sys.argv[j])
+    print(add)
